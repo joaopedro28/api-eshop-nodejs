1a61aa4f880c3c116e367a1a624b628a688817fb
Joao Pedro Rocha <joaopedro@wdhouse.com.br>
2021-02-16 19:44:12 -0300

    Finish api eshop

---
+++ models/category.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String,
+        required: false
+    },
+    color: {
+        type: String,
+        required: false
+    },
+
+})
+categorySchema.virtual('id').get(function() {
+	return this._id.toHexString();
+})
+
+categorySchema.set('toJSON', {
+	virtuals: true
+})
+
+exports.Category = mongoose.model('Category', categorySchema)
